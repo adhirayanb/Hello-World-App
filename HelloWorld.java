5992b188fdb744f883af494e095518f2d5fa8118
Adhirayan Baskar <ab7531@srmist.edu.in>
2026-03-26 10:53:59 +0530

    uc6:compliment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,25 @@
 public class HelloWorld {
-
     public static void main(String[] args) {
-
+        // Check if no arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
-        }
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
 
-        StringBuilder nameBuilder = new StringBuilder();
+            // Iterate through arguments using an enhanced for loop
+            for (String name : args) {
+                // Always append the same delimiter after each name
+                nameBuilder.append(name).append(", ");
+            }
 
-        for (int i = 0; i < args.length; i++) {
-            nameBuilder.append(args[i]);
-
-            if (i < args.length - 1) {
-                nameBuilder.append(", ");
+            // Check length to avoid exceptions before applying substring()
+            if (nameBuilder.length() > 0) {
+                // Extract everything except the last two characters (", ")
+                String formattedNames = nameBuilder.substring(0, nameBuilder.length() - 2);
+                
+                // Print the final greeting
+                System.out.println("Hello, " + formattedNames + "!");
             }
         }
-
-        String names = nameBuilder.toString();
-
-        System.out.println("Hello, " + names + "!");
     }
 }
