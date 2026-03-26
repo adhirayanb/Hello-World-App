8a41bda867d8af785efdbf102bd6d7e0ab8a6520
Adhirayan Baskar <ab7531@srmist.edu.in>
2026-03-26 10:15:39 +0530

    UC3:complited

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,24 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello");
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
+
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+
+            if (i < args.length - 1) {
+                nameBuilder.append(", ");
+            }
+        }
+
+        String names = nameBuilder.toString();
+
+        System.out.println("Hello, " + names + "!");
     }
 }
